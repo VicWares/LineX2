8be5955d073b08031923474f63612a313b6cba4c
Vic Wintriss <vic@wintrisstech.org>
2023-02-23 12:32:56 -0800

    Way to get values into JFree chart

---
+++ src/main/java/org/linechartexample/LineChartWithCategoryDatasetExample.java
@@ -0,0 +1,55 @@
+package org.linechartexample;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+public class LineChartWithCategoryDatasetExample extends JFrame
+{
+    public static JFreeChart chart;
+    public static DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+    public LineChartWithCategoryDatasetExample()//Constructor
+    {
+        super("LineX2 version 230222");
+        JPanel chartPanel = createChartPanel();
+        add(chartPanel, BorderLayout.CENTER);
+        setSize(1800, 1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                new LineChartWithCategoryDatasetExample().setVisible(true);
+                extracted();
+            }
+        });
+    }
+    public static void extracted()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            dataset.addValue(i, "Epoch", (i) + "");
+        }
+    }
+    public static JPanel createChartPanel()
+    {
+        String chartTitle = "Iris Classifier";
+        String categoryAxisLabel = "Epoch";
+        String valueAxisLabel = "RMS Error";
+        boolean showLegend = false;
+        boolean createURL = false;
+        boolean createTooltip = false;
+        chart = ChartFactory.createLineChart(chartTitle,
+                categoryAxisLabel, valueAxisLabel, dataset,
+                PlotOrientation.VERTICAL, showLegend, createTooltip, createURL);
+        return new ChartPanel(chart);
+    }
+}
